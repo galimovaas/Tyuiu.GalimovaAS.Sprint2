738f9541465b48afd8e73115804ccbc53f7d1dc9
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-10-16 23:29:20 +0500

    Task4

--- Tyuiu.GalimovaAS.Sprint2.Task4.V17.Lib/DataService.cs
+++ Tyuiu.GalimovaAS.Sprint2.Task4.V17.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.GalimovaAS.Sprint2.Task4.V17.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = x - 22 < Math.Sqrt(y) ? (Math.Pow((6 + 2 / (x * x)), y)) : (y + 12 / (x * x));
+            double z = x - 22 > Math.Sqrt(y) ? (Math.Pow((6 + 2 / (x * x)), y)) : (y + 12 / (x * x));
             return Math.Round(z, 3);
 
         }
